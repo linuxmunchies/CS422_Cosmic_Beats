ee834a9cecb0cf5420b38ae5dfae13150a14b898
Chia Wei (Jasper) Shen <59499543+jasper060@users.noreply.github.com>
2024-07-29 03:55:19 -0500

    Update MainActivity.kt

--- musicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.kt
+++ musicPlayer/app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.musicplayer
 
+import android.R.attr.onClick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,6 +15,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
@@ -74,11 +77,34 @@ fun MusicPlayerApp() {
                 arguments = listOf(navArgument("playlistId") { type = NavType.IntType })
             ) { backStackEntry ->
                 val playlistId = backStackEntry.arguments?.getInt("playlistId") ?: 0
-                PlaylistDetailScreen(playlistId, Modifier.padding(innerPadding))
+                PlaylistDetailScreen(
+                    playlistId,
+                    onSongClick = { songTitle, artistName ->
+                        navController.navigate("songDetail/$songTitle/$artistName")
+                    },
+                    Modifier.padding(innerPadding)
+                )
+            }
+            composable(
+                "songDetail/{songTitle}/{artistName}",
+                arguments = listOf(
+                    navArgument("songTitle") { type = NavType.StringType },
+                    navArgument("artistName") { type = NavType.StringType }
+                )
+            ) { backStackEntry ->
+                val songlistId = backStackEntry.arguments?.getInt("songlistId") ?: 0
+                val songTitle = backStackEntry.arguments?.getString("songTitle") ?: ""
+                val artistName = backStackEntry.arguments?.getString("artistName") ?: ""
+                SongDetailScreen(
+                    songlistId,
+                    songTitle,
+                    artistName,
+                    Modifier.padding(innerPadding))
             }
         }
     }
 }
+
 
 @Composable
 fun PlaylistsScreen(onPlaylistClick: (Int) -> Unit, modifier: Modifier = Modifier) {
@@ -106,7 +132,10 @@ fun PlaylistsScreen(onPlaylistClick: (Int) -> Unit, modifier: Modifier = Modifie
 }
 
 @Composable
-fun PlaylistDetailScreen(playlistId: Int, modifier: Modifier = Modifier) {
+fun PlaylistDetailScreen(
+    playlistId: Int,
+    onSongClick: (String, String) -> Unit,
+    modifier: Modifier = Modifier) {
     val songs = remember {
         listOf(
             "No Scrubs" to "TLC",
@@ -138,7 +167,8 @@ fun PlaylistDetailScreen(playlistId: Int, modifier: Modifier = Modifier) {
                 ListItem(
                     headlineContent = { Text(title, maxLines = 1, overflow = TextOverflow.Ellipsis) },
                     supportingContent = { Text(artist, maxLines = 1, overflow = TextOverflow.Ellipsis) },
-                    leadingContent = { Icon(Icons.Filled.MusicNote, contentDescription = null) }
+                    leadingContent = { Icon(Icons.Filled.MusicNote, contentDescription = null) },
+                    modifier = Modifier.clickable { onSongClick(title, artist) }
                 )
                 HorizontalDivider()
             }
@@ -189,3 +219,166 @@ fun BottomPlayerBar() {
         }
     )
 }
+
+
+@Composable
+fun SongDetailScreen(songlistId: Int, songTitle: String, artistName: String, modifier: Modifier = Modifier) {
+    var isPlaying by remember { mutableStateOf(false) }
+    var sliderPosition by remember { mutableStateOf(0f) }
+    var sliderPosition1 by remember { mutableStateOf(0f) }
+    var sliderPosition2 by remember { mutableStateOf(0f) }
+    var sliderPosition3 by remember { mutableStateOf(0f) }
+    var sliderPosition4 by remember { mutableStateOf(0f) }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = songTitle,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        Text(
+            text = artistName,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = 0f..1f,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Text(
+                text = "60 Hz ${(sliderPosition * 100).toInt()}%",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Slider(
+                value = sliderPosition1,
+                onValueChange = { sliderPosition1 = it },
+                valueRange = 0f..1f,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Text(
+                text = "250 Hz ${(sliderPosition1 * 100).toInt()}%",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Slider(
+                value = sliderPosition2,
+                onValueChange = { sliderPosition2 = it },
+                valueRange = 0f..1f,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Text(
+                text = "1k Hz ${(sliderPosition2 * 100).toInt()}%",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Slider(
+                value = sliderPosition3,
+                onValueChange = { sliderPosition3 = it },
+                valueRange = 0f..1f,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Text(
+                text = "4k Hz ${(sliderPosition3 * 100).toInt()}%",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Slider(
+                value = sliderPosition4,
+                onValueChange = { sliderPosition4 = it },
+                valueRange = 0f..1f,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            )
+            Text(
+                text = "16k Hz ${(sliderPosition4 * 100).toInt()}%",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(
+                text = "Equalizer Bar",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+//            IconButton(onClick = { /* Previous song action */ }) {
+//                Icon(Icons.Filled.SkipPrevious, contentDescription = "Skip to previous")
+//            }
+//            IconButton(onClick = { isPlaying = !isPlaying }) {
+//                Icon(
+//                    if (isPlaying) Icons.Filled.Pause else Icons.Filled.PlayArrow,
+//                    contentDescription = if (isPlaying) "Pause" else "Play"
+//                )
+//            }
+//            IconButton(onClick = { /* Next song action */ }) {
+//                Icon(Icons.Filled.SkipNext, contentDescription = "Skip to next")
+//            }
+            ElevatedButton(onClick = {  }) {
+                Text("Flat")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            ElevatedButton(onClick = {  }) {
+                Text("Bass Boot")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            ElevatedButton(onClick = {  }) {
+                Text("Treble Boost")
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            ElevatedButton(onClick = {  }) {
+                Text("Save")
+            }
+        }
+    }
+}
+
